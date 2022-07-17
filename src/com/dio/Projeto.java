c24d180fb459d2798443b55531918f32e4e93ce8
regi1297 <reginald_miranda@hotmail.com>
2022-07-17 16:29:50 -0300

    First commit

---
+++ src/com/dio/Projeto.java
@@ -0,0 +1,15 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class Projeto {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+       /* int a = 2;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+}
